2bf8f19d7e43ed7be34ccd4979f9bc2a1a429d03
lihuijie <huijie.li@greencloud.com.cn>
2025-08-20 23:31:38 +0800

    feat: 优化 WebSocket 连接管理并添加用户信息提取功能
    [- 新增 ConnectionManager 类，用于优化连接的管理和监控
    - 添加 UserInfoExtractor 类，实现从多种方式提取用户信息
    - 更新 MyWebSocketHandler，集成新的连接管理和用户信息提取逻辑
    - 新增连接监控 API 接口，提供在线用户数、总连接数等统计信息
    - 优化线程池配置，提高处理性能
    - 更新依赖，替换 Forest 为 SimpleRequest]

--- web/src/main/java/com/lidachui/websocket/web/controller/MessageSendApi.java
+++ web/src/main/java/com/lidachui/websocket/web/controller/MessageSendApi.java
@@ -1,11 +1,8 @@
 package com.lidachui.websocket.web.controller;
 
 
-import com.alibaba.fastjson.JSONObject;
 import com.lidachui.websocket.api.request.WebSocketMessageRequest;
 import com.lidachui.websocket.common.result.Result;
-import com.lidachui.websocket.common.util.SpringUtil;
-import com.lidachui.websocket.manager.client.GitHubClient;
 import com.lidachui.websocket.service.MessageSendService;
 import javax.annotation.Resource;
 import javax.validation.Valid;
@@ -30,8 +27,6 @@ public class MessageSendApi {
      */
     @PostMapping("/pushMessage/message")
     public Result<String> sendMessage(@RequestBody @Valid WebSocketMessageRequest request) {
-        GitHubClient bean = SpringUtil.getBean(GitHubClient.class);
-        JSONObject lidachui1998 = bean.getUser("lidachui1998");
         messageSendService.sendMessage(request);
         return Result.success("发送成功!");
     }
